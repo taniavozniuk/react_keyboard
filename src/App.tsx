a1887ca2ac713d95d34f0d077ac0380e8702b6d2
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-23 11:24:04 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  lastkey: string | null;
+}
+
+// export const App: React.FC = () => (
+//   <div className="App">
+//     <p className="App__message">The last pressed key is [Enter]</p>
+//   </div>
+
+//   componetDidMount() : void {
+//     document.addListener()
+//   }
+// );
+
+export class App extends Component<{}, State> {
+  state: State = {
+    lastkey: null,
+  };
+
+  handleDocumentRightClick = (event: KeyboardEvent): void => {
+    this.setState({ lastkey: event.key });
+    console.log(event.key);
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleDocumentRightClick);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleDocumentRightClick);
+  }
+
+  render() {
+    const { lastkey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          The last pressed key is {lastkey ? `[${lastkey}]` : 'Enter'}
+        </p>
+      </div>
+    );
+  }
+}
